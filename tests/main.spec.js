3b86f6f54e30da5cefed3bb5ecc8fa263d7c9319
Felipe <alvespatriciofelipe@gmail.com>
2017-12-18 23:01:19 -0300

    Criação de main.spec.js para testes - instalação do Mocha e do chai

---
+++ tests/main.spec.js
@@ -0,0 +1,21 @@
+describe('', () => {
+  // roda uma vez antes do bloco
+  before(() => {
+
+  });
+
+  // Roda uma vez depois do bloco
+  after(() => {
+
+  });
+
+  // Roda todas as vezes, antes de cada bloco
+  beforeEach( () => {
+
+  });
+
+  // Roda todas as vezes, após cada bloco
+  afterEach(() => {
+
+  });
+});
