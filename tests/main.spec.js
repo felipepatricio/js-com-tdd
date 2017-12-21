4942ef12153257102a0ae108890896cae38d1407
Felipe <alvespatriciofelipe@gmail.com>
2017-12-21 01:02:04 -0300

    Teste para calculadora simples. E Metodos da calculadora

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,41 +1,26 @@
 const expect = require('chai').expect;
+const calc = require('../src/main.js');
 
-describe('Main', () => {
-  let arr;
-  // roda uma vez antes do bloco
-  before(() => {
-    // inicia a conexão no banco
-  });
-
-  // Roda uma vez depois do bloco
-  after(() => {
-    // fecha a conexão no banco
-  });
-
-  // Roda todas as vezes, antes de cada bloco
-  beforeEach( () => {
-    arr = [1, 2, 3];
-  });
-
-  // Roda todas as vezes, após cada bloco
-  afterEach(() => {
-
-  });
-  it('Should be an array', () => {
-    expect(arr).to.be.a('array');
-  })
-
-  it('Should have a size of 4 when push another value to array', () => {
-    arr.push(4);
-    expect(arr).to.have.lengthOf(4); // 4
-  });
-
-  it('Should have a size of 2 when pop a value from the array', () => {
-    arr.pop();
-    expect(arr).to.length(2); // 2
-  });
-
-  it('Should remove 3 when user pop another the array ', () => {
-    expect(arr).to.include(3); // true
+describe('Calc', () => {
+  describe('smoke test', () => {
+    it('Should exists the calc lib', () => {
+      expect(calc).to.exist;
+    });
+    it('Should exists method `sum`', () => {
+      expect(calc.sum).to.exist;
+      expect(calc.sum).to.be.a('function');
+    });
+    it('Should exists method `sub`', () => {
+      expect(calc.sub).to.exist;
+      expect(calc.sub).to.be.a('function');
+    });
+    it('Should exists method `mult`', () => {
+      expect(calc.mult).to.exist;
+      expect(calc.mult).to.be.a('function');
+    });
+    it('Should exists method `div`', () => {
+      expect(calc.div).to.exist;
+      expect(calc.div).to.be.a('function');
+    })
   });
 });
