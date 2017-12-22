46a1e563440ace3187a1f79e05d5f32c6ce3a9ec
Felipe <alvespatriciofelipe@gmail.com>
2017-12-22 06:59:45 -0300

    Criação o .babelrc, e refatoração para es6

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,25 +1,23 @@
-const expect = require('chai').expect;
-const calc = require('../src/main.js');
+import { expect } from 'chai';
+
+import { sum, sub, div, mult } from '../src/main';
 
 describe('Calc', () => {
   describe('smoke test', () => {
-    it('Should exists the calc lib', () => {
-      expect(calc).to.exist;
-    });
     it('Should returned 4 when `sum(2, 2)`', () => {
-      expect(calc.sum(2, 2)).to.be.equal(4);
+      expect(sum(2, 2)).to.be.equal(4);
     });
     it('Should returned 0 when `sub(2, 2)`', () => {
-      expect(calc.sub(2, 2)).to.be.equal(0);
+      expect(sub(2, 2)).to.be.equal(0);
     });
     it('Should returned 1 when `div(2, 2)`', () => {
-      expect(calc.div(2, 2)).to.be.equal(1);
+      expect(div(2, 2)).to.be.equal(1);
     });
     it('Should returned "Não é possível divisão por 0" when `div(2, 0)', () => {
-      expect(calc.div(2, 0)).to.be.equal("Não é possível divisão por 0");
+      expect(div(2, 0)).to.be.equal("Não é possível divisão por zero");
     });
     it('Should returned 4 when `mult(2, 2)', () => {
-      expect(calc.mult(2, 2)).to.be.equal(4);
+      expect(mult(2, 2)).to.be.equal(4);
     });
   });
 });
